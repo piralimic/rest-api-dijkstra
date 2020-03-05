a2a370cb7a2e9028f56bf8165e9a258b9e10a79d
piralimic <pirali.michael@gmail.com>
2020-03-05 15:51:55 +0100

    [change] results route

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :countries do
-        post '/results' => 'countries#result'
         resources :cities
         resources :ways
       end
+      post '/results' => 'countries#result'
     end
   end
 end
